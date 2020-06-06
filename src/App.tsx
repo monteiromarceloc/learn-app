e128caa10b2d5b2ce9df2fc0ca9a0b39c47ba2f7
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-06-06 20:29:29 -0300

    search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { MdSearch } from "react-icons/md";
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello
-        </p>
+        <div className='Search-box'>
+          <MdSearch />
+          <input
+            placeholder='O que você quer aprender?'
+            autoFocus
+          />
+        </div>
       </header>
     </div>
   );
